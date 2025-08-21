0163cf090c3949d027d64675a1894c2044ad77ac
sakun915 <poojasakunde08@gmail.com>
2025-08-21 08:29:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import Sidebar from './components/Sidebar';
 import Header from './components/Header';
-import Home from './pages/Home';
 import AnalyticsPage from './pages/Analytics/AnalyticsPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ErrorBoundary from './ErrorBoundary';
 
 const App = () => {
@@ -11,21 +9,19 @@ const App = () => {
     <Router>
       <div className="flex flex-col min-h-screen">
         <Header />
-        <div className="flex flex-1">
-          <Sidebar />
-          <div className="flex-1 p-4 bg-gray-50 overflow-auto">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route 
-                path="/analytics" 
-                element={
-                  <ErrorBoundary>
-                    <AnalyticsPage />
-                  </ErrorBoundary>
-                } 
-              />
-            </Routes>
-          </div>
+        <div className="flex-1 p-4 bg-gray-50 overflow-auto">
+          <Routes>
+            {/* Redirect "/" to "/analytics" */}
+            <Route path="/" element={<Navigate to="/analytics" replace />} />
+            <Route 
+              path="/analytics" 
+              element={
+                <ErrorBoundary>
+                  <AnalyticsPage />
+                </ErrorBoundary>
+              } 
+            />
+          </Routes>
         </div>
       </div>
     </Router>
